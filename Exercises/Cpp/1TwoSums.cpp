c3c536bcf1b8009b24a33008653482a8b5ac1911
VERITASSSSSSS <68550874+VERITASSSSSSS@users.noreply.github.com>
2021-07-24 23:25:38 +0800

    Update 1TwoSums.cpp

--- Exercises/Cpp/1TwoSums.cpp
+++ Exercises/Cpp/1TwoSums.cpp
@@ -18,4 +18,40 @@ public:
             }
 };
 
+***************************************************************************************
+
+// 1) target-nums[]
+
+//Runtime: 43 ms, faster than 37.51% of C++ online submissions for Two Sum.
+//Memory Usage: 14.3 MB, less than 6.69% of C++ online submissions for Two Sum.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //2(0) 7(1) 11(2) 15(3)
+        
+        // [3,4] 6 if 6-3 = if 3 exists is not 3 then i found answer
+        map<int,int>myMap;
+        vector<int> arr(2);
+        
+        //0(n)
+        for(int i=0;i<nums.size();i++)
+            myMap[nums[i]]=i; //saving index to value in map
+            // 2 equals 0; 7 equals 1...
+        
+        //0(n)
+        for(int i=0;i<nums.size();i++){
+            int comp = target - nums[i];
+            // if 7 exsists and value 7 is not itself, found
+            if(myMap[comp]&& myMap[comp]!=i){
+                arr[0] = i; //index 
+            arr[1] = myMap[comp];
+            }
+        }
+        return arr;
+    }
+};
+
+//https://www.youtube.com/watch?v=v1qXNTTGpyo&t=904s
+
 
